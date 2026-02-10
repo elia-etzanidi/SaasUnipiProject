cdc492785d5b70d2cb6ef053b2997c76fd04c4ad
Elia Etzanidi <eliaetz@gmail.com>
2026-02-10 12:55:14 +0200

    fix: correct auth header name

--- SaasPlatform/Backend/routes/user.route.js
+++ SaasPlatform/Backend/routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const auth = require('../middleware/auth');
 
 // SIGN UP
 router.post('/register', userController.registerUser);
